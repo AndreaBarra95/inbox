a128b64fc23a88e0cd6074c94b17af632730c063
ANDREA BARRA <andrea.barra001@studenti.uniparthenope.it>
2022-06-05 13:37:52 +0200

    first commit

---
+++ test/Inbox.test.js
@@ -0,0 +1,54 @@
+// contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+    //Get a list of all available accounts
+    accounts = await web3.eth.getAccounts();
+
+    // Use one of those accounts to deploy the contract on testnet
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ 
+            data: bytecode, 
+            arguments: ['Hello There!'] })
+        .send({ from: accounts[0], gas: '1000000' })
+    
+});
+
+describe('Inbox Smart Contract', () => {
+    it('contract deployment', () => {
+        assert.ok(inbox.options.address);
+    });
+
+    it('a default message is initialized', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hello There!');
+    });
+
+    it('setMessage function should set a new message', async () => {
+        await inbox.methods.setMessage('Hello buddy!').send({from: accounts[0], gas: '1000000'});
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hello buddy!');
+    });
+
+    it('getMessage function should return a message string', async () => {
+        await inbox.methods.getMessage().send({from: accounts[0], gas: '1000000'});
+        const message = await inbox.methods.getMessage().call();
+        assert.equal(message, 'Hello There!');
+    });
+
+    it('Testing setMessage and getMessage together', async () => {
+        await inbox.methods.setMessage('Ciao bello').send({ from: accounts[0], gas: '1000000' });
+        const message = await inbox.methods.message().call();
+        await inbox.methods.getMessage().send({ from: accounts[0], gas: '1000000' });
+        const newMessage = await inbox.methods.getMessage().call();
+        assert.equal(newMessage, message);
+    } );
+
+});
